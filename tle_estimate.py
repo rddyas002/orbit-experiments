8068e08a0a91d46e48ab336e8213c3406df3af3c
Yashren Reddi <yreddi@spaceteq.co.za>
2018-11-28 11:51:22 +0200

    complete tle estimator

--- tle_estimate.py
+++ tle_estimate.py
@@ -10,15 +10,9 @@ from sgp4.propagation import sgp4init, sgp4
 from sgp4.model import Satellite
 
 import csv
-from math import pi, pow
+from math import pi, pow, sqrt
 
-#def rosen(x):
-    #"""The Rosenbrock function"""
-    #return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
-#
-#x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
-#bounds = Bounds([0, 0, 0, 0, 0], [2.0, 2.0, 2.0, 2.0, 2.0])
-#res = minimize(rosen, x0, method='nelder-mead', options={'xtol': 1e-8, 'disp': True},bounds=bounds)
+import time
 
 def sgp4_compute(jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar,delta_min):
     deg2rad = pi/180.0;         
@@ -83,13 +77,57 @@ def read_csv_data(filepath):
     np_v_gps = np.array(v_gps)    
     return np_t, np_x_gps, np_v_gps
 
-def get_sgp4_propagation(np_t,jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar,delta_min):
+def get_sgp4_propagation(np_t,jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar):
+    deg2rad = pi/180.0;         
+    xpdotp = 1440.0/(2.0*pi);
+
+    satrec = Satellite();
+    satrec.error = 0;
+    whichconst = wgs84;
+    satrec.whichconst = whichconst;
+    tumin = whichconst.tumin
+    satrec.satnum = 8195;
+    satrec.jdsatepoch = jdsatepoch
+    satrec.no = no;
+    satrec.ecco = ecco;
+    satrec.inclo = inclo;
+    satrec.nodeo = nodeo;
+    satrec.argpo = argpo;
+    satrec.mo = mo;
+    satrec.nddot = 0.00000e0;
+    satrec.bstar = bstar;
+    satrec.ndot = 0.000000;
+
+    satrec.a = pow(satrec.no*tumin, (-2.0/3.0));
+    satrec.alta = satrec.a*(1.0 + satrec.ecco) - 1.0;
+    satrec.altp = satrec.a*(1.0 - satrec.ecco) - 1.0;
+    
+    satrec.elnum = 813;
+    satrec.revnum = 22565;
+    satrec.classification = 'U';
+    satrec.intldesg = '75081A';
+    satrec.ephtype = 0;
+
+    # convert units and initialize
+    satrec.no = satrec.no / xpdotp; 
+    satrec.ndot  = satrec.ndot  / (xpdotp*1440.0);  
+    satrec.nddot = satrec.nddot / (xpdotp*1440.0*1440);
+    satrec.inclo = satrec.inclo  * deg2rad;
+    satrec.nodeo = satrec.nodeo  * deg2rad;
+    satrec.argpo = satrec.argpo  * deg2rad;
+    satrec.mo    = satrec.mo     * deg2rad;
+
+    afspc_mode = False
+
+    sgp4init(whichconst, afspc_mode, satrec.satnum, satrec.jdsatepoch-2433281.5, satrec.bstar, satrec.ecco, satrec.argpo, satrec.inclo, satrec.mo, satrec.no, satrec.nodeo, satrec)
+    
     x_sgp4 = []
     v_sgp4 = []
 
     for t in np_t:
         delta_min = t/60
-        [r,v] = sgp4_compute(jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar,delta_min)
+        #[r,v] = sgp4_compute(jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar,delta_min)
+        [r,v] = sgp4(satrec,delta_min)
         x_sgp4.append(r)
         v_sgp4.append(v)
 
@@ -97,29 +135,95 @@ def get_sgp4_propagation(np_t,jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar,delt
     np_v_sgp4 = np.array(v_sgp4)
     return np_t, np_x_sgp4, np_v_sgp4
 
+def sgp4_error(x,jdsatepoch,np_t,r_gps,v_gps):
+    e_sq = 0
+    weighting = np.arange(len(np_t),0,-1)
+    [np_t, np_x_sgp4, np_v_sgp4] = get_sgp4_propagation(np_t,jdsatepoch,x[0],x[1],x[2],x[3],x[4],x[5],x[6])
+
+    pos_error = np.linalg.norm(np.transpose(r_gps - np_x_sgp4), axis=0)
+    vel_error = np.linalg.norm(np.transpose(v_gps - np_v_sgp4), axis=0)
+    pos_err_weighted = np.multiply(pos_error,weighting)
+    vel_err_weighted = np.multiply(pos_error,weighting)
+    pos_err_sq = np.square(pos_err_weighted)
+    vel_err_sq = np.square(vel_err_weighted)
+    err = sqrt(np.sum(pos_err_sq)) + sqrt(np.sum(vel_err_sq))
+
+    return err
+
+def sgp4_error_print(x,jdsatepoch,np_t,r_gps,v_gps):
+    e_sq = 0
+    weighting = np.arange(len(np_t),0,-1)
+    [np_t, np_x_sgp4, np_v_sgp4] = get_sgp4_propagation(np_t,jdsatepoch,x[0],x[1],x[2],x[3],x[4],x[5],x[6])
+
+    pos_error = np.linalg.norm(np.transpose(r_gps - np_x_sgp4), axis=0)
+    vel_error = np.linalg.norm(np.transpose(v_gps - np_v_sgp4), axis=0)
+    plt.plot(np_t/84600,pos_error)
+    plt.show()
+    pos_err_weighted = np.multiply(pos_error,weighting)
+    pos_err_sq = np.square(pos_err_weighted)
+    err = sqrt(np.sum(pos_err_sq))
+
+    return err
+        
+
 year = 2016
 [mon,day,hr,minute,sec] = days2mdhms(year,111.34375000)
 jdsatepoch = jday(year,mon,day,hr,minute,sec)
-ecco = 85.1260441997737e-006;
-inclo = 97.5200151325645e+000;
-nodeo = 185.845569746093e+000;
-argpo = 106.542369211203e+000;
-mo = 273.306829557493e+000;
-no = 15.1105221216793e+000;
-bstar = 1.06508772985060e-003;
-delta_min = 0
+ecco = 0.001
+inclo = 97.5
+nodeo = 185.0
+argpo = 106.0
+mo = 273.0
+no = 15.11
+bstar = 1.06508772985060e-004
 
-bounds = Bounds([0, 96, 180, 0, 0, 15, 1e-6], [0.1, 99, 190, 360, 360, 16, 1e-2])
+ecco = 8.373941923917806e-05
+inclo = 97.51977154369243
+nodeo = 185.84556875835602
+argpo = 136.12512076314346
+mo = 243.7244849621949
+no = 15.110521668513265
+bstar = 0.0010642643733820103
+
+print('Initial parameters:')
+print('ecc = {0}'.format(ecco))
+print('inclo = {0}'.format(inclo))
+print('nodeo = {0}'.format(nodeo))
+print('argpo = {0}'.format(argpo))
+print('mo = {0}'.format(mo))
+print('no = {0}'.format(no))
+print('bstar = {0}'.format(bstar))
+
+x0 = np.array([ecco,inclo,nodeo,argpo,mo,no,bstar])
+
+bounds = Bounds([0, 96, 180, 0, 0, 15, 1e-6], [0.1, 99, 190, 360, 360, 16, 1e-1])
 
 filepath = './/data//sat_gps.csv'
 
 [np_t, np_x_gps, np_v_gps] = read_csv_data(filepath)
-[np_t, np_x_sgp4, np_v_sgp4] = get_sgp4_propagation(np_t,jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar,delta_min)
+#err = sgp4_error_print(x0,jdsatepoch,np_t,np_x_gps,np_v_gps)
 
-np_error = np.absolute(np_x_sgp4 - np_x_gps)
+start = time.time()
+print('Running optimisation...')
+res = minimize(sgp4_error, x0, args=(jdsatepoch,np_t,np_x_gps,np_v_gps), method='nelder-mead', options={'xtol': 1e-12, 'disp': True})
+end = time.time()
+print('Time taken for minimization: {0:2f} seconds'.format(end-start))
+#res = minimize(sgp4_error, x0, args=(jdsatepoch,np_t,np_x_gps,np_v_gps), method='L-BFGS-B', options={'disp': True},bounds=bounds)
+if 'res' in locals():
+    print('minimize operation succeed: {0}'.format(res.success))
+    print('ecco = {0}'.format(res.x[0]))
+    print('inclo = {0}'.format(res.x[1]))
+    print('nodeo = {0}'.format(res.x[2]))
+    print('argpo = {0}'.format(res.x[3]))
+    print('mo = {0}'.format(res.x[4]))
+    print('no = {0}'.format(res.x[5]))
+    print('bstar = {0}'.format(res.x[6]))
+#[np_t, np_x_sgp4, np_v_sgp4] = get_sgp4_propagation(np_t,jdsatepoch,ecco,inclo,nodeo,argpo,mo,no,bstar)
 
-plt.title('GPS vs. SGP4 error')
-plt.plot(np_t/84600,np_error)
+#np_error = np.absolute(np_x_sgp4 - np_x_gps)
+
+#plt.title('GPS vs. SGP4 error')
+#plt.plot(np_t/84600,np_error)
 #plt.plot(np_t/84600,np_x_sgp4,'--')
-plt.grid()
-plt.show()
+#plt.grid()
+#plt.show()
