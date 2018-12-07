67a1c89a78288b5c3a03d92061887a8f3aba3e63
Yashren Reddi <yashren.reddi@uct.ac.za>
2018-12-07 23:19:57 +0200

    update

--- aSGP4_init.m
+++ aSGP4_init.m
@@ -11,5 +11,13 @@ GMAT_simdata.signals.values = GMAT(:,5:10);
 orbit_start_UTC = [2019 01 20 09 00 00];
 
 Orbit_period = 24*60*60/KEP_TLE(7);     % seconds
-sim_stop = Orbit_period*1*KEP_TLE(7);
+GPS_on_percent = 0.05;
+GPS_time_sequence = [0 Orbit_period*GPS_on_percent Orbit_period*GPS_on_percent+1 Orbit_period/2-1 Orbit_period/2 Orbit_period/2+Orbit_period*GPS_on_percent Orbit_period/2+Orbit_period*GPS_on_percent+1 Orbit_period];
+GPS_value_sequence = [1 1 0 0 1 1 0 0];
+%GPS_time_sequence = [0 Orbit_period*GPS_on_percent Orbit_period*GPS_on_percent+1 Orbit_period];
+%GPS_value_sequence = [1 1 0 0];
+sim_stop = Orbit_period*5*KEP_TLE(7);
 sim_stop_max = max(GMAT_simdata.time);
+mean_motion = KEP_TLE(7)*2*pi/(24*60*60);
+mu = 398600.4415;
+semi_major = (mu/mean_motion^2)^(1/3);
